000ec31017844c17bee4dad1b8aa4d2ea56d92df
Viktory4121 <medvedka4121@gmail.com>
2023-05-19 18:42:28 +0300

    add tab's

---
+++ script.js
@@ -0,0 +1,39 @@
+//=================ПРИМЕР==============================
+//мягкий скролл к определённому блоку:
+/* document.getElementById("main-action").onclick = function() {
+    document.getElementById("cars").scrollIntoView({behavior: "smooth"});
+} */
+
+document.getElementById("tab-home-button").onclick = function() {
+    document.getElementById("tab-home").scrollIntoView({behavior: "smooth"});
+}
+document.getElementById("tab-home-button2").onclick = function() {
+    document.getElementById("tab-home").scrollIntoView({behavior: "smooth"});
+}
+
+
+document.getElementById("tab-about-button").onclick = function() {
+    document.getElementById("tab-about").scrollIntoView({behavior: "smooth"});
+}
+
+
+document.getElementById("tab-service-button").onclick = function() {
+    document.getElementById("tab-service").scrollIntoView({behavior: "smooth"});
+}
+document.getElementById("tab-service-button2").onclick = function() {
+    document.getElementById("tab-service").scrollIntoView({behavior: "smooth"});
+}
+
+
+document.getElementById("tab-portfolio-button").onclick = function() {
+    document.getElementById("tab-portfolio").scrollIntoView({behavior: "smooth"});
+}
+
+
+document.getElementById("tab-contact-button").onclick = function() {
+    document.getElementById("tab-contact").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("tab-contact-button2").onclick = function() {
+    document.getElementById("tab-contact").scrollIntoView({behavior: "smooth"});
+}
